5b81c63affcd9617fe9ad16722adf861e0eb8d37
Glenn Searby <glenn.searby@gmail.com>
2014-12-15 13:31:08 +0000

    Added search field and results to the dialogue. Still need to implement the actual search XHR call.

--- Gadget-process-model.js
+++ Gadget-process-model.js
@@ -5,6 +5,8 @@
 (function() {
     var title = "visualeditor-mwprocessmodel-title",
 	dialogueName = "Process Model Dialogue",
+	processModelCollection = "process-models",
+	queryUrl = blah,
 
 	translations = {
 	    en: {
@@ -29,15 +31,50 @@
     dialogue.static.name = dialogueName;
     dialogue.static.titleMessage = title;
 
-
     dialogue.prototype.initialize = function() {
 	ve.ui.Dialog.prototype.initialize.call(this);
-	// TODO make the dialogue do things
-    };
 
-    // TODO work out if I need to do this.
-    // dialogue.prototype.onDocumentTransact = function() {
-    // };
+	var search = new ve.ui.SearchWidget(),
+	    doSearch = function(value, callback) {
+		makeSearchRequest(function(results, error) {
+		    if (value === search.query.value) {
+			if (error) {
+			    throw new Error(error);
+			} else {
+			    callback(results);
+			}
+			
+		    } else {
+			// Noop, our search is out of date.
+		    }
+		});
+	    };
+
+	search.query.on("change", function() {
+	    doSearch(search.query.value, function(results) {
+		search.results.clearItems();
+		search.results.addItems(
+		    results.map(function(r) {
+			
+			return new ve.ui.OptionWidget(
+			    r,
+			    {
+				// Ensure we have a String for the label.
+				label: "" + r
+			    }
+			);
+		    })
+		);
+	    });
+	});
+
+	search.on("select", function(data) {
+	    alert("Selected " + data);
+	});
+
+	this.$body.append(search.$query);
+	this.$body.append(search.$results);
+    };
 
     ve.ui.dialogFactory.register(dialogue);
 

e169c7609c5fc6f8659824ee5cdd1e779f7ecaa4
Glenn Searby <glenn.searby@gmail.com>
2014-12-16 15:02:58 +0000

    Added an entry in Special:Version for my extension.

--- steep-mediawiki-gadgets.php
+++ steep-mediawiki-gadgets.php
@@ -13,6 +13,16 @@ $wgResourceModules['ext.steep-mediawiki-gadgets'] = array(
     'remoteExtPath' => 'steep-mediawiki-gadgets'
 );
 
+$wgExtensionCredits['steep-mediawiki-gadgets'][] = array(
+    'path' => __FILE__,
+    'name' => 'Steep Extensions',
+    'author' => 'Glenn Searby', 
+    'url' => 'https://github.com/cse-bristol/steep-mediawiki-gadgets', 
+    'description' => 'This extension adds the ability to include Process Models and Energy Efficiency Maps in a wiki page.',
+    'version'  => 0.1,
+    'license-name' => "NUT",
+);
+
 array_push($wgVisualEditorPluginModules, 'ext.steep-mediawiki-gadgets');
 
 ?>  

fa54f08c738b4e7130b514b093147dd7f485484d
Glenn Searby <glenn.searby@gmail.com>
2014-12-16 14:29:41 +0000

    Fixed internationalization.

--- steep-mediawiki-gadgets.php
+++ steep-mediawiki-gadgets.php
@@ -1,9 +1,14 @@
 <?php
 
+$wgMessagesDirs['ViewFiles'] = __DIR__ . '/i18n';
+
 $wgResourceModules['ext.steep-mediawiki-gadgets'] = array(
     'scripts' => array( 'js/insert-process-model.js'),
     'messages' => array('visualeditor-mwprocessmodel-title'),
-    'dependencies' => array('ext.visualEditor.core'),
+    'dependencies' => array(
+        'ext.visualEditor.core',
+        'mediawiki.jqueryMsg'
+    ),
     'localBasePath' => __DIR__,
     'remoteExtPath' => 'steep-mediawiki-gadgets'
 );

797b3195ea5d117ea0514af5038b56c8d668d80e
Glenn Searby <glenn.searby@gmail.com>
2015-06-11 14:50:25 +0100

    Got rid of unhelpful pulls.

--- scripts/update-steep-server-components.sh
+++ scripts/update-steep-server-components.sh
@@ -9,18 +9,15 @@ echo "Updating the steep server side components.";
 
 git -C "${PROCESS_MODEL_DIR}" fetch;
 git -C "${PROCESS_MODEL_DIR}" checkout "${PROCESS_MODEL_VERSION}";
-git -C "${PROCESS_MODEL_DIR}" pull;
 make -C "${PROCESS_MODEL_DIR}";
 
 git -C "${MAP_DIR}" fetch;
 git -C "${MAP_DIR}" checkout "${MAP_VERSION}";
-git -C "${MAP_DIR}" pull;
 git -C "${MAP_DIR}" submodule update --init;
 make -C "${MAP_DIR}";
 
 git -C "${SHARE_DIR}" fetch;
 git -C "${SHARE_DIR}" checkout "${SHARE_VERSION}";
-git -C "${SHARE_DIR}" pull;
 make -C "${SHARE_DIR}";
 
 echo "Set up share server as a SystemD service.";

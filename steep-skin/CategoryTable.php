657665c0cd9c00e3e910b064d0c602cc7970ccd2
Centre for Sustainable Energy <cse@steep.cse>
2015-09-02 18:21:50 +0100

    added pagination

--- steep-skin/CategoryTable.php
+++ steep-skin/CategoryTable.php
@@ -8,6 +8,12 @@
    We do this by both hooking into and extending CategoryPage.
  */
 class CategoryTable extends Article {
+  // This many results per page.
+  private $pageSize = 10;
+  // Our current page.
+  private $page = 1;
+  // We'll show this many pages either side of the current page, if they're available.
+  private $pagesToShow = 3;
 
   public static function hook() {
     $wgHooks['CategoryPageView'][] = 'CategoryTable::drawCategoryAsTable';
@@ -21,6 +27,12 @@ class CategoryTable extends Article {
 
     $tablePage->setContext($categoryPage->getContext());
     $tablePage->setParserOptions($categoryPage->getParserOptions());
+
+    $page = $tablePage->getContext()->getRequest()->getInt('page');
+
+    if ($page) {
+      $tablePage->page = $page;
+    }
 
     /*
        Article also includes the following internal state, which we haven't copied over.
@@ -37,9 +49,9 @@ class CategoryTable extends Article {
        // mParseOutput
      */
 
-   
+    
     $tablePage->view();
-   
+    
     /*
        Returning false causes the original CategoryPage to return from its view method without doing anything itself.
      */
@@ -52,7 +64,7 @@ class CategoryTable extends Article {
   
   function view() {
     $this->getContext()->getOutput()->enableOOUI();
-    
+
     $this->out(
       new OOUI\ButtonWidget(
 	array(
@@ -89,7 +101,8 @@ class CategoryTable extends Article {
 	  'action' => 'query',
 	  'list' => 'search',
 	  'srsearch' => 'incategory:' . $this->getTitle()->getText(),
-	  'srlimit' => 10,
+	  'srlimit' => $this->pageSize,
+	  'sroffset' => $this->pageSize * ($this->page - 1),
 	  'srnamespace' => join(
 	    '|',
 	    array(
@@ -126,6 +139,8 @@ class CategoryTable extends Article {
 	)
       )
     );
+
+    $this->pagination($api->getResult()->getResultData()['query']['searchinfo']['totalhits']);
     
     parent::view();
 
@@ -183,6 +198,81 @@ class CategoryTable extends Article {
   function cell($content, $params = array()) {
     return Html::rawElement(
       'td',
+      $params,
+      $content
+    );
+  }
+
+  function pagination($hits) {
+    if ($hits <= $this->pageSize) {
+      return;
+    }
+
+    $lastPage = ceil($hits / $this->pageSize);
+
+    $pageLinks = '';
+
+    if ($this->page > 1) {
+      $pageLinks .= $this->paginationLink($this->page - 1, 'Prev');
+
+      $pagesBefore = range(
+	max(
+	  $this->page - $this->pagesToShow,
+	  1
+	),
+	$this->page - 1
+      );
+
+      foreach ($pagesBefore as $i => $p) {
+	$pageLinks .= $this->paginationLink($p);
+      };      
+    }
+
+    $pageLinks .= Html::rawElement(
+      'span',
+      array(),
+      $this->page . ' of ' . $lastPage
+    );
+
+    if ($this->page < $lastPage) {
+      $pagesAfter = range(
+	$this->page + 1,
+	min(
+	  $this->page + $this->pagesToShow,
+	  $lastPage
+	)
+      );
+      
+      foreach ($pagesAfter as $p) {
+	$pageLinks .= $this->paginationLink($p);
+      }
+
+      $pageLinks .= $this->paginationLink($this->page + 1, 'More');
+    }
+
+    $this->out(
+      Html::rawElement(
+	'div',
+	array(
+	  'class' => 'pagination'
+	),
+	$pageLinks
+      )
+    );
+  }
+
+  function paginationLink($page, $content = null, $params = array()) {
+    $query = $this->getContext()->getRequest()->getQueryValues();
+    $query['page'] = $page;
+    
+    $params['href'] = "?" . http_build_query($query);
+
+    if (!$content) {
+      $content = $page;
+    }
+    
+    return Html::rawElement(
+      'a',
       $params,
       $content
     );

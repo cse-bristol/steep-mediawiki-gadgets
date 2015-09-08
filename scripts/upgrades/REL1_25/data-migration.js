d039f1f084385301fe7d8358071b5b840d49445d
Glenn Searby <glenn.searby@gmail.com>
2015-09-08 17:17:59 +0100

    Drop elasticsearch index at beginning of migration.

--- scripts/upgrades/REL1_25/data-migration.js
+++ scripts/upgrades/REL1_25/data-migration.js
@@ -4,7 +4,7 @@
 
 var mongoClient = require('mongodb').MongoClient,
     
-    elasticClient = require('livedb-elasticsearch')(null, 'share'),
+    elasticClient = require('livedb-elasticsearch')(null, 'share', true),
 
     collections = [
 	'demo',
@@ -26,70 +26,84 @@ var mongoClient = require('mongodb').MongoClient,
     };
 
 mongoClient.connect('mongodb://localhost:27017/share', function(error, db) {
+    elasticClient.deleteMappings(function(error, result) {
+	if (error) {
+	    console.error("dropping mappings", error);
+	    process.exit(1);
+	} else {
+	    elasticClient.ensureMappingsCreated(function(error, result) {
+		if (error) {
+		    console.error("creating mappings", error);
+		    process.exit(1);
+		} else {
+		    collections.forEach(function(c) {
+			var snapshots = db.collection(c),
+			    opsName = c + "_ops",
+			    ops = db.collection(opsName);
 
-    collections.forEach(function(c) {
-	var snapshots = db.collection(c),
-	    opsName = c + "_ops",
-	    ops = db.collection(opsName);
+			todo[c] = {};
+			todo[opsName] = {};
 
-	todo[c] = {};
-	todo[opsName] = {};
+			snapshots.find({}).toArray(function(error, docs) {
+			    docs.forEach(function(snapshot) {
+				todo[c][snapshot._id] = true;
 
-	snapshots.find({}).toArray(function(error, docs) {
-	    docs.forEach(function(snapshot) {
-		todo[c][snapshot._id] = true;
+				var body = {
+				    v: snapshot._v,
+				    type: snapshot._type,
+				    data: {}
+				};
 
-		var body = {
-		    v: snapshot._v,
-		    type: snapshot._type,
-		    data: {}
-		};
+				Object.keys(snapshot).forEach(function(k) {
+				    if (k[0] !== "_") {
+					body.data[k] = snapshot[k];
+				    }
+				});
+				
+				elasticClient.writeSnapshot(
+				    c,
+				    snapshot._id,
+				    body,
+				    function(error, result) {
+					if (error) {
+					    console.error(c, snapshot, error);
+					}
+					triggerEnd(c, snapshot._id);
+				    }
+				); 
+			    });
+			});
+			
+			ops.find({}).toArray(function(error, docs) {
+			    docs.forEach(function(op) {
+				todo[opsName][op._id] = true;
 
-		Object.keys(snapshot).forEach(function(k) {
-		    if (k[0] !== "_") {
-			body.data[k] = snapshot[k];
-		    }
-		});
-		
-		elasticClient.writeSnapshot(
-		    c,
-		    snapshot._id,
-		    body,
-		    function(error, result) {
-			if (error) {
-			    console.error(c, snapshot, error);
-			}
-			triggerEnd(c, snapshot._id);
-		    }
-		); 
+				elasticClient.writeOp(
+				    opsName,
+				    op._id,
+				    {
+					v: op._v,
+					src: op.src,
+					seq: op.seq,
+					meta: op.m,
+					op: op.op,
+					create: op.create,
+					del: op.del
+				    },
+				    function(error, result) {
+					if (error) {
+					    console.error(opsName, op, error);
+					}
+					triggerEnd(opsName, op._id);
+				    }
+				);
+			    });
+			});
+		    });
+		}
 	    });
-	});
-	
-	ops.find({}).toArray(function(error, docs) {
-	    docs.forEach(function(op) {
-		todo[opsName][op._id] = true;
-
-		elasticClient.writeOp(
-		    opsName,
-		    op._id,
-		    {
-			v: op._v,
-			src: op.src,
-			seq: op.seq,
-			meta: op.m,
-			op: op.op,
-			create: op.create,
-			del: op.del
-		    },
-		    function(error, result) {
-			if (error) {
-			    console.error(opsName, op, error);
-			}
-			triggerEnd(opsName, op._id);
-		    }
-		);
-	    });
-	});
+	}
     });
 });
+
 

bc866669529b38e17b45e12c8fa265205ae5d240
TrickyBytes <richard.tiffin@akoolla.com>
2015-09-14 15:43:05 +0100

    Fix to insert correct wiki text when inserting a process model via visual editor, still need make this work for map and probably move to parent SteepNode

--- js/ve.dm.ProcessModelNode.js
+++ js/ve.dm.ProcessModelNode.js
@@ -14,6 +14,20 @@ OO.inheritClass(ve.dm.ProcessModelNode, ve.dm.SteepNode);
 ve.dm.ProcessModelNode.static.matchRdfaTypes = ['mw:Extension/process-model'];
 ve.dm.ProcessModelNode.static.name = "process-model";
 
+ve.dm.ProcessModelNode.static.toDomElements = function ( dataElement, doc ) {
+	if ( this.matchTagNames && dataElement.type == 'process-model') {
+	        var processModel = doc.createElement ( 'process-model' )
+
+	        processModel.setAttribute("name", dataElement.name);
+	        processModel.setAttribute("version", dataElement.v);
+	        processModel.setAttribute("width", dataElement.width);
+	        processModel.setAttribute("height", dataElement.height);
+
+	        return [processModel];
+	}
+	throw new Error( 've.dm.Model subclass must match a single tag name or implement toDomElements' );
+};
+
 ve.dm.ProcessModelNode.static.toDataElement = function(domElements, converter) {
     var d = ve.dm.SteepNode.static.toDataElementHelper.apply(this, arguments);
     d.type = ve.dm.ProcessModelNode.static.name;

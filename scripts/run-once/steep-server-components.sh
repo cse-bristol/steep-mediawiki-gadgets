c9038c9971b59405343eab9de01793fd70c012b6
Glenn Searby <glenn.searby@gmail.com>
2015-09-15 10:13:31 +0100

    Use https:// instead of git@ clone to get our repositories.
    Allows these to be updated without using credentials.

--- scripts/run-once/steep-server-components.sh
+++ scripts/run-once/steep-server-components.sh
@@ -7,14 +7,14 @@ set -e;
 
 echo "Clone the repositories for the various Steep components.";
 if ! [ -d "${PROCESS_MODEL_DIR}" ]; then
-    sudo git clone "git@github.com:cse-bristol/process-model.git" "${PROCESS_MODEL_DIR}";
+    sudo git clone "https://@github.com/cse-bristol/process-model.git" "${PROCESS_MODEL_DIR}";
 fi;
 
 if ! [ -d "${MAP_DIR}" ]; then
-    sudo git clone "git@github.com:cse-bristol/energy-efficiency-planner.git" "${MAP_DIR}";
+    sudo git clone "https://github.com/cse-bristol/energy-efficiency-planner.git" "${MAP_DIR}";
 fi;
 if ! [ -d "${SHARE_DIR}" ]; then
-    sudo git clone "git@github.com:cse-bristol/share-server.git" "${SHARE_DIR}";
+    sudo git clone "https://github.com/cse-bristol/share-server.git" "${SHARE_DIR}";
 fi;
 
 echo "Sort out permissions so that both the current user and www-data can read and write to them.";

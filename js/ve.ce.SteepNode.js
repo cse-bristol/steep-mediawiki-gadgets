75475fa69f517f3aa3c168cedd7e2879ec0ff94c
TrickyBytes <richard.tiffin@akoolla.com>
2015-09-15 11:41:46 +0100

    Fix: Incorrect code in content editable item

--- js/ve.ce.SteepNode.js
+++ js/ve.ce.SteepNode.js
@@ -1,63 +1,50 @@
 "use strict";
 
-/*global OO, ve*/
+/*global ve, OO, $*/
 
-/*
- A Visual Editor node which represents an XML tag used by the Steep extension.
- */
-ve.dm.SteepNode = function SteepNode(options) {
-    ve.dm.SteepNode.super.apply(this, arguments);
+ve.ce.SteepNode = function SteepNode(model, config) {
+    ve.ce.LeafNode.call(this, model, config);
 
-    if (options) {
-	    this.name = options.name;
-	    this.v = options.v;
-	    this.width = options.width;
-	    this.height = options.height;
+    this.$element = $('<iframe/>')
+        .attr('typeof', model.type)
+	      .attr('src', this.buildSrc(model))
+	      .css("overflow", "hidden");
+
+    if (model.width) {
+	      this.$element.css("width", model.width);
+    }
+
+    if (model.height) {
+	      this.$element.css("height", model.height);
     }
 };
 
-OO.inheritClass(ve.dm.SteepNode, ve.dm.LeafNode);
+OO.inheritClass(ve.ce.SteepNode, ve.ce.LeafNode);
 
-ve.dm.SteepNode.static.matchTagNames = ['iframe'];
-ve.dm.SteepNode.static.isContent = true;
-ve.dm.SteepNode.static.enableAboutGrouping = true;
+ve.ce.SteepNode.prototype.buildSrc = function(model) {
+    var url = "/" + this.toolUrl() + "/?name=" + encodeURIComponent(model.name),
+	args = this.srcArgs(model);
 
-ve.dm.SteepNode.static.toDomElements =  function ( dataElement, doc ) {
-    if ( this.matchTagNames && dataElement.type == this.name) {
-	        var processModel = doc.createElement ( this.name )
+    args.v = model.v;
 
-	        processModel.setAttribute("name", dataElement.name);
-	        processModel.setAttribute("version", dataElement.v);
-	        processModel.setAttribute("width", dataElement.width);
-	        processModel.setAttribute("height", dataElement.height);
-	        
-	        return [processModel];
-    }
-    throw new Error( 've.dm.Model subclass must match a single tag name or implement toDomElements' );
-};
+    Object.keys(args).forEach(function(k) {
+	var val = args[k];
 
-ve.dm.SteepNode.static.toDataElementHelper = function(domElements, converter) {
-    var iframe = domElements[0],
-	data = JSON.parse(
-	    iframe.getAttribute("data-mw"));
-
-    return {
-	name: data.attrs.name,
-	v: data.attrs.v,
-	width: data.attrs.width,
-	height: data.attrs.height
-    };
-};
-
-ve.dm.SteepNode.prototype.toLinearModel = function() {
-    var data = {
-	    type: this.type,
-	    name: this.name,
-	    v: 8,
-	    width: this.width,
-	    height: this.height
-    };
+	if (val === null || val === undefined || val === "") {
+	    // No-op
+	} else {
+	    url += "&" + k + "=" + encodeURIComponent(val);
+	}
+    });
     
-    this.addModelData(data);
-    return [data];
+    return url;
 };
+
+ve.ce.SteepNode.prototype.toolUrl = function() {
+    throw new Error("Not implemented: tooUrl should be overriden in subtype.");
+};
+
+ve.ce.SteepNode.prototype.srcArgs = function(model) {
+    throw new Error("Not implemented: srcArgs should be overridden in subtype.");
+};
+

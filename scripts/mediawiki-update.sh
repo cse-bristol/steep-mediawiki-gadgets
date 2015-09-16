b83af3e30764d692a5bf25a6cf61571a96b5660a
Glenn Searby <glenn.searby@gmail.com>
2015-09-16 11:49:25 +0100

    Install script changes:  + Run PHP scripts with the Mediawiki dir as the current working  directory. Fixes some problems with the R extension not being clever  enough to work this out.  + Call runJobs.php to clear out the jobs queue.

--- scripts/mediawiki-update.sh
+++ scripts/mediawiki-update.sh
@@ -11,8 +11,11 @@ echo "Granting write permission on images folder.";
 sudo chown -R "${USER}":steep "${NEW_DIR}/images";
 sudo chmod g+sw -R "${NEW_DIR}/images";
 
+# We need to do this (in particular the rebuildData.php script) from the mediawiki directory, because something inside the R-extension doesn't check its path properly.
+pushd "${NEW_DIR}" > /dev/null;
+
 echo "Running Mediawiki's update script (sorts out the database tables).";
-php "${NEW_DIR}/maintenance/update.php";    
+php "${NEW_DIR}/maintenance/update.php";
 
 echo "Refreshing Semantic Data";
 php "${NEW_DIR}/extensions/SemanticMediaWiki/maintenance/rebuildData.php";
@@ -21,3 +24,8 @@ php "${NEW_DIR}/extensions/SemanticMediaWiki/maintenance/rebuildData.php";
 echo "Setting up CirrusSearch indexes.";
 php "${EXT_DIR}/CirrusSearch/maintenance/updateSearchIndexConfig.php";
 php "${EXT_DIR}/CirrusSearch/maintenance/forceSearchIndex.php"
+
+# Run all outstanding jobs.
+php "${NEW_DIR}/maintenance/runJobs.php";
+
+popd > /dev/null;

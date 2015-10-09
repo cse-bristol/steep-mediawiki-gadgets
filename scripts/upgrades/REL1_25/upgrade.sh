ab01612d5ff7cbf2d990a0ce4fa0e9eae4f98d1c
Glenn Searby <glenn.searby@gmail.com>
2015-10-09 10:31:41 +0100

    SteepSettings.php is now symlinked into place instead of copied. This makes for easier updating.

--- scripts/upgrades/REL1_25/upgrade.sh
+++ scripts/upgrades/REL1_25/upgrade.sh
@@ -9,4 +9,10 @@ set -e;
 # Symlink it into the skins directory.
 ln -s -T "${STEEP_DIR}/steep-skin" "${NEW_DIR}/skins/Steep";
 
+# Make sure SteepSettings.php is symlinked into place.
+rm -f "${NEW_DIR}/${EXTRA_CONFIG_FILE}";
+ln -s -T "${STEEP_DIR}/scripts/${EXTRA_CONFIG_FILE}" "${NEW_DIR}/${EXTRA_CONFIG_FILE}";
+
+# Migrate from MongoDB to ElasticSearch.
 source "search.sh";
+

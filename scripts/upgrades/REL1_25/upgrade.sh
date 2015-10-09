f3cb3e7ba609366c12f4b157d2f9342efd80481d
Glenn Searby <glenn.searby@gmail.com>
2015-10-09 10:38:56 +0100

    Previous change to SteepSettings.php is now done in a separate file for easier testing.

--- scripts/upgrades/REL1_25/upgrade.sh
+++ scripts/upgrades/REL1_25/upgrade.sh
@@ -9,9 +9,7 @@ set -e;
 # Symlink it into the skins directory.
 ln -s -T "${STEEP_DIR}/steep-skin" "${NEW_DIR}/skins/Steep";
 
-# Make sure SteepSettings.php is symlinked into place.
-rm -f "${NEW_DIR}/${EXTRA_CONFIG_FILE}";
-ln -s -T "${STEEP_DIR}/scripts/${EXTRA_CONFIG_FILE}" "${NEW_DIR}/${EXTRA_CONFIG_FILE}";
+source "steep-settings-symlink.sh";
 
 # Migrate from MongoDB to ElasticSearch.
 source "search.sh";
